94ee4753f0132cd6663bb8890570537b2343d5ee
Miguelito2926 <juniorprol@hotmail.com>
2023-12-02 14:48:59 -0300

    Create projeto, componentes e definindo as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
+import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component';
+import { CategoriaDetalheComponent } from './categoria-detalhe/categoria-detalhe.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'categorias',
+    component:CategoriasComponent,
+    data:{title: 'Categorias'}
+},
+{
+    path:'categoria-nova',
+    component:CategoriaNovaComponent,
+    data:{title: 'Nova Categoria'}
+},
+{
+    path:'categoria-detalhe/:id',
+    component:CategoriaDetalheComponent,
+    data:{title: 'Detalhes da Categoria'}
+},
+{
+    path:'categoria-editar/:id',
+    component:CategoriaEditarComponent,
+    data:{title: 'Edita a Categoria'}
+},
+{
+    path:'login',
+    component:LoginComponent,
+    data:{title: 'Login'}
+},
+{
+    path:'logout',
+    component:LogoutComponent,
+    data:{title: 'Logout'}
+},
+{ path: '',
+redirectTo: '/categorias',
+pathMatch: 'full'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { CategoriaNovaComponent } from './categoria-nova/categoria-nova.component';
+import { CategoriaDetalheComponent } from './categoria-detalhe/categoria-detalhe.component';
+import { CategoriaEditarComponent } from './categoria-editar/categoria-editar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './menu/menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    CategoriasComponent,
+    CategoriaNovaComponent,
+    CategoriaDetalheComponent,
+    CategoriaEditarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [
     provideClientHydration()
